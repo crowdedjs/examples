ed97d9bd5208d847dc359eda70ead4c681aa7c14
B. Ricks <bricks@unomaha.edu>
2021-01-25 05:23:41 -0600

    Fix import statement

--- support/Room.js
+++ support/Room.js
@@ -2,15 +2,15 @@ import LocationStatus from "./location-status.js"
 
 class Room {
 
-	constructor(location, roomType, name) {
-    this.name = name;
-    this.location = location;
-    this.roomType = roomType;
-    this.locationStatus = LocationStatus.NONE;
-    this.locationStatusOwner = null;		
-	}
+    constructor(location, roomType, name) {
+        this.name = name;
+        this.location = location;
+        this.roomType = roomType;
+        this.locationStatus = LocationStatus.NONE;
+        this.locationStatusOwner = null;
+    }
 
-	
+
 
 }
 
