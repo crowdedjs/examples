2fdbeb7ecade50304eb1aeca44079e4306a8939f
B. Ricks <bricks@unomaha.edu>
2021-02-20 11:10:43 -0600

    Simplify selenium tests

--- test/selenium.test.js
+++ test/selenium.test.js
@@ -20,7 +20,8 @@ before("Start server", async function () {
     if (err) return console.error(err);
   })
   driver = await new Selenium.Builder().forBrowser('chrome').build();
-      
+  driver.get("http://localhost:8127");
+
 })
 
 after("End server", async function () {
@@ -32,22 +33,10 @@ describe("It builds and runs", function () {
   describe("Selenium Tests", async function () {
     it("Has the correct title", async function () {
       this.timeout(100000);
-      try {
-        await driver.get("http://localhost:8127");
-        await driver.wait(Selenium.until.titleIs('Single Crowd Simulator'), 1000);
-        return driver.findElement(Selenium.By.id("ComputerEntryTable"))
-          .then(result =>{
-            expect(true).to.be.true
-          })
-         
-        
-      } catch (e) {
-        console.error(e);
-      }
-      finally {
-        
-      }
-      expect(true).to.be.true;
+      return driver.wait(Selenium.until.titleIs('Single Crowd Simulator'), 1000);
+    })
+    it("Has the table element", async function () {
+      return driver.findElement(Selenium.By.id("ComputerEntryTable"))
     })
   })
 })
